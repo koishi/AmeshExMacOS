7ad2d57f121f4b2ae3fb13b29cd4b490b49a1c26
Koichiro Oishi <bs@v7.com>
2017-05-01 14:39:04 +0900

    Initial Commit

---
+++ AmeshExMacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AmeshExMacOS
+//
+//  Created by 大石弘一郎 on 2017/05/01.
+//  Copyright © 2017年 大石弘一郎. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AmeshExMacOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AmeshExMacOS
+//
+//  Created by 大石弘一郎 on 2017/05/01.
+//  Copyright © 2017年 大石弘一郎. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
